7bba6631b7f88922d8f93e44c43697ce70962c5d
Edward Tanguay (windows) <edward@tanguay.info>
2023-08-23 14:21:41 +0200

    almost finished

---
+++ index.js
@@ -0,0 +1,13 @@
+import readlineSync from 'readline-sync';
+
+const userName = readlineSync.question('What is your name? ');
+const bornYear = readlineSync.question('In which year were you born? ');
+const bornCity = readlineSync.question('In which city were you born? ');
+const currentCity = readlineSync.question('Where do you live now? ');
+const currentYear = readlineSync.question('What is the current year? ');
+
+const currentYearNum = Number(currentYear);
+const bornYearNum = Number(bornYear);
+const agoYears = currentYearNum - bornYearNum; 
+
+console.log(`Hello! My name is ${userName}. I was born ${agoYears} years ago in London. Now I live in Berlin.`);
